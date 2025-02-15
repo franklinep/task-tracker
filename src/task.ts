ea760d52e2a15087af3f80e6e6a1740022784c1b
Franklin <franklin.espinoza0226@gmail.com>
2025-02-15 12:45:12 -0500

    Agregar tipo TaskStatus e interfaz Task en task.ts

---
+++ src/task.ts
@@ -0,0 +1,11 @@
+export type TaskStatus = 'todo' | 'in-progress' | 'done';
+export interface Task {
+    id: number;
+    description?: string;
+    status?: TaskStatus;
+    createdAt: Date;
+    updatedAt: Date;
+}
+
+
+
