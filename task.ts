ab544560d9aab3eeba7c04d2248f82376c683fad
Franklin <franklin.espinoza0226@gmail.com>
2025-02-15 12:44:45 -0500

    eliminar: Se movio los archivos a /src

--- task.ts
+++
@@ -1,9 +0,0 @@
-type TaskStatus = 'todo' | 'in-progress' | 'done';
-export interface Task {
-    id: number;
-    description: string;
-    status: TaskStatus;
-    createdAt: Date;
-    updatedAt: Date;
-}
-
